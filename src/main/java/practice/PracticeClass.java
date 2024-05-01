949b220127d76ecfe1f124441cf03175a4105eb4
maheshkvenkat <venkatmahesh808@gmail.com>
2024-05-01 21:50:16 +0530

    Main project

---
+++ src/main/java/practice/PracticeClass.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class PracticeClass {
+	
+	public void test1()
+	{
+		System.out.println("This is Test1 Method");
+	}
+	
+	public static void main(String[] args)
+	{
+		PracticeClass obj=new PracticeClass();
+		obj.test1();
+	}
+
+}
